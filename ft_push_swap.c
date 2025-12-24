7c08b7838212054f4bcbded195e71e86baadc6f6
Adam Raji <adraji@e3r1p17.1337.ma>
2025-12-24 15:12:47 +0100

    remove teast in main file

--- ft_push_swap.c
+++ ft_push_swap.c
@@ -6,25 +6,11 @@
 /*   By: adraji <adraji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/20 10:44:03 by adraji            #+#    #+#             */
-/*   Updated: 2025/12/24 15:09:07 by adraji           ###   ########.fr       */
+/*   Updated: 2025/12/24 15:12:16 by adraji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
-
-void	ft_print_stack(t_head *head)
-{
-	while (head)
-	{
-		ft_printf("********************\n");
-		ft_printf("|value :%11d|\n", head->value);
-		ft_printf("|how to far :%6d|\n", head->how_far);
-		ft_printf("|value index :%5d|\n", head->value_index);
-		ft_printf("|stack index :%5d|\n", head->stack_index);
-		head = head->rear;
-	}
-	ft_printf("********************\n");
-}
 
 int	main(int c, char **v)
 {
