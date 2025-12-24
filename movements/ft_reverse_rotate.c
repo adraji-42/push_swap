ce35d783b9e71444c54600909cd24d9e606a0a3a
Adam Raji <adraji@e3r10p15.1337.ma>
2025-12-24 12:34:47 +0100

    completed movements stack

--- movements/ft_reverse_rotate.c
+++ movements/ft_reverse_rotate.c
@@ -1,50 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*    ft_rotate.c                                       :+:      :+:    :+:   */
+/*   ft_reverse_rotate.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adraji <adraji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/24 10:15:24 by adraji            #+#    #+#             */
-/*   Updated: 2025/12/24 12:26:29 by adraji           ###   ########.fr       */
+/*   Created: 2025/12/24 11:59:56 by adraji            #+#    #+#             */
+/*   Updated: 2025/12/24 12:33:40 by adraji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_push_swap.h"
 
-static void	ft_rotate(t_stack *stack)
+static void	ft_reverse_rotate(t_stack *stack)
 {
 	t_head	*rear;
-	t_head	*second;
+	t_head	*penultimate;
 
-	if (!stack->head || stack->size < 2)
+	if (!stack || !stack->head || !stack->head->rear)
 		return ;
 	rear = stack->head;
-	second = stack->head->rear;
 	while (rear->rear)
 		rear = rear->rear;
+	penultimate = rear->top;
 	rear->rear = stack->head;
 	stack->head->top = rear;
-	stack->head->rear = NULL;
-	stack->head = second;
-	stack->head->top = NULL;
+	penultimate->rear = NULL;
+	stack->head = rear;
+	rear->top = NULL;
 }
 
-void	ft_ra(t_stack *a)
+void	ft_rra(t_stack *a)
 {
-	ft_rotate(a);
-	ft_printf("ra\n");
+	ft_reverse_rotate(a);
+	ft_printf("rra\n");
 }
 
-void	ft_rb(t_stack *b)
+void	ft_rrb(t_stack *b)
 {
-	ft_rotate(b);
-	ft_printf("rb\n");
+	ft_reverse_rotate(b);
+	ft_printf("rrb\n");
 }
 
-void	ft_rr(t_stack *a, t_stack *b)
+void	ft_rrr(t_stack *a, t_stack *b)
 {
-	ft_rotate(a);
-	ft_rotate(b);
-	ft_printf("rr\n");
+	ft_reverse_rotate(a);
+	ft_reverse_rotate(b);
+	ft_printf("rrr\n");
 }
